f7bd4b833ada48c272173d17ffcc14a20b0d615e
Raafat Douban <147871439+RaafatDouban@users.noreply.github.com>
2026-05-10 02:24:18 +0300

    Game Done

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void Button_Start(){
+        SceneManager.LoadScene("GameLevel01");
+    }
+
+    public void Button_Medium(){
+        SceneManager.LoadScene("GameLevel02");
+    }
+
+    public void Button_Hard(){
+        SceneManager.LoadScene("GameLevel03");
+    }
+
+    public void Button_Levels(){
+        SceneManager.LoadScene("Levels");
+    }
+
+    public void Button_MainMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Button_Quit(){
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+
+        Application.Quit();
+
+    }
+}
